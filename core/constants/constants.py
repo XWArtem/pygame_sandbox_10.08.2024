0022e331e838a8179ebe0e6d90eac3495a117a27
XWArtem <xwartem@gmail.com>
2024-08-10 01:55:23 +0200

    Player imitation addded
    Red rect square added as the Player

--- core/constants/constants.py
+++ core/constants/constants.py
@@ -2,3 +2,4 @@ import pygame
 import sys
 
 BG_COLOR = (135, 206, 235)
+PLAYER_COLOR = (255, 0, 0)
--- core/main.py
+++ core/main.py
@@ -2,13 +2,18 @@ import pygame
 import sys
 
 from constants.constants import BG_COLOR
+from constants.constants import PLAYER_COLOR
 
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 800
 
+screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
+
+player = pygame.Rect(300, 250, 50, 50)
+
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
+    
     pygame.display.set_caption('Sandbox game')
     clock = pygame.time.Clock()
 
@@ -20,7 +25,9 @@ def main():
                 sys.exit()
 
         screen.fill(BG_COLOR)
-        pygame.display.flip()
+        pygame.draw.rect(screen, PLAYER_COLOR, player)
+        
+        pygame.display.update()
         clock.tick(60)
 
 if __name__ == "__main__":
